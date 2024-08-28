ffa1c04038245e7f41c3389d0cc21554317d01d8
areebahasnain <142868074+areebahasnain@users.noreply.github.com>
2024-08-28 10:06:22 +0500

    Add files via upload

---
+++ Labs/02/Q6.py
@@ -0,0 +1,11 @@
+def multiply_list(numbers):
+    result = 1
+    
+    for number in numbers:
+        result *= number
+    
+    return result
+
+numbers = [1, 2, 3, 4, 5]  
+product = multiply_list(numbers)
+print("Product of the list =", product)
