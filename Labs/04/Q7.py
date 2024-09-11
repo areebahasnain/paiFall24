310fdb7140a2a88f52e9f4039ca9db94b290bc4b
areebahasnain <142868074+areebahasnain@users.noreply.github.com>
2024-09-11 09:53:40 +0500

    Add files via upload

---
+++ Labs/04/Q7.py
@@ -0,0 +1,22 @@
+class Vehicle:
+    def __init__(self, seating_capacity):
+        self.seating_capacity = seating_capacity
+
+    def calculate_fare(self):
+        return self.seating_capacity * 100
+
+class Bus(Vehicle):
+    def __init__(self, seating_capacity):
+        super().__init__(seating_capacity)
+    
+    def calculate_fare(self):
+        base_fare = super().calculate_fare()
+        maintenance_charge = 0.10 * base_fare
+        total_fare = base_fare + maintenance_charge
+        return total_fare
+
+vehicle = Vehicle(seating_capacity = 40)
+print(f"Default fare for Vehicle: Rs.{vehicle.calculate_fare()}")
+
+bus = Bus(seating_capacity = 40)
+print(f"Total fare for Bus: Rs.{bus.calculate_fare()}")
